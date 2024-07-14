b7fb4c87972743a5794a2a718f7ff32b83a09e6c
Sarim Bin Asif <sarimbinasif12345@gmail.com>
2024-07-14 20:47:15 +0500

    added edit todo

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,24 +1,27 @@
-import { useContext, createContext } from "react";
+import {createContext, useContext} from "react";
 
-// inside createContext there are default values and fucntion
 export const TodoContext = createContext({
     todos: [
         {
             id: 1,
-            todoMsg: "Todo list text",
-            taskCompleted: false,
+            todo: " Todo msg",
+            completed: false,
         }
     ],
-    addTodo: (todoMsg) => {},
-    editTodo: (id, todoMsg) => {},
-    deleteTodo: (id) => {},
-    toggleComplete: (id) => {}
-})
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {
+        console.log(`Update todo with id: ${id}, new todo: ${todo}`);
+    },
+    deleteTodo: (id) => {
+        console.log(`Delete todo with id: ${id}`);
+    },
+    toggleComplete: (id) => {
+        console.log(`Toggle complete for todo with id: ${id}`);
+    }
+});
 
-export const TodoProvider = TodoContext.Provider
-
-// using useTodo() we can access  todos[], addTodo(),editTodo(),
-// deleteTodo(), toggleComplete()
-export default function useTodo(){
-    return useContext(TodoContext)
+export const useTodo = () => {
+    return useContext(TodoContext);
 }
+
+export const TodoProvider = TodoContext.Provider;
